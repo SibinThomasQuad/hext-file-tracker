665f4205ce3af280a0f7b0b1352a0d0f5b8faa95
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2022-04-27 14:51:46 +0530

    Update hex_and_hash_md5.py

--- hex_and_hash_md5.py
+++ hex_and_hash_md5.py
@@ -2,26 +2,33 @@ import binascii
 import os
 import hashlib
 class Calculate                                     : 
+    
     def hex(self,file_name)                         : 
         filename                                    = file_name
         with open(filename, 'rb') as f              : 
             content                                 = f.read()
         return binascii.hexlify(content)
+    
     def md5_sum(self,file_name)                     : 
         md5_hash                                    = hashlib.md5(open(file_name,'rb').read()).hexdigest()
         return md5_hash
+
 class Compare                                       : 
     claculate_obj                                   = Calculate()
+    
     def file_with_hex(self,file_path,old_hex)       : 
         file_hex                                    = str(self.claculate_obj.hex(file_path))
         if old_hex in file_hex                      : 
             print("[+][HEX] [WARNING] [Target HEX CODE FOUND] ["+file_path+"]")
+    
     def file_with_md5(self,file_path,old_hash)      : 
         file_hash                                   = self.claculate_obj.md5_sum(file_path)
         if(file_hash == old_hash):
             print("[+][MD5] [WARNING] [Target MD5 CODE FOUND] ["+file_path+"]")
+
 class Scan                                          : 
     compare_obj                                     = Compare()
+    
     def all_files(self,dir_name,target_hex,type)    : 
         root                                        = dir_name
         for path, subdirs, files in os.walk(root)   : 
@@ -31,9 +38,11 @@ class Scan                                          :
                     self.compare_obj.file_with_hex(file_path,target_hex)
                 if(type == 'md5'):
                     self.compare_obj.file_with_md5(file_path,target_hex)
+
 def run(directory,hexcode,type)                     : 
     scan_obj                                        = Scan()
     scan_obj.all_files(directory,hexcode,type)
+
 def container()                                     : 
     label                                           = '''
      _   _ _______  __   _____ ____   _  ___  _ 
@@ -65,6 +74,8 @@ def container()                                     :
         code                                        = rule_code.read()
         exec(code)
         print('[+] Scanning Completed')
+
+
 try                                                 : 
     container()
 except                                              : 
