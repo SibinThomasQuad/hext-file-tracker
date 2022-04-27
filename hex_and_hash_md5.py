cb6e96fc65c37cb222b7b73012aadcac7a5b2521
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2022-04-27 14:42:17 +0530

    Update hex_and_hash_md5.py

--- hex_and_hash_md5.py
+++ hex_and_hash_md5.py
@@ -15,11 +15,11 @@ class Compare:
     def file_with_hex(self,file_path,old_hex):
         file_hex = str(self.claculate_obj.hex(file_path))
         if old_hex in file_hex:
-            print("[+] ['WARNING'] [Target HEX CODE FOUND] ["+file_path+"]")
+            print("[+][HEX] [WARNING] [Target HEX CODE FOUND] ["+file_path+"]")
     def file_with_md5(self,file_path,old_hash):
         file_hash = self.claculate_obj.md5_sum(file_path)
         if(file_hash == old_hash):
-            print("[+] ['WARNING'] [Target MD5 CODE FOUND] ["+file_path+"]")
+            print("[+][MD5] [WARNING] [Target MD5 CODE FOUND] ["+file_path+"]")
 class Scan:
     compare_obj = Compare()
     def all_files(self,dir_name,target_hex,type):
@@ -45,10 +45,13 @@ def container():
     '''
     print(label)
     claculate_obj = Calculate()
-    print("[1] [Calculate Hex]\n")
-    print("[2] [Calculate MD5]\n")
-    print("[3] [Scan for Rule]\n")
+    print("[1] [Calculate Hex]")
+    print("[2] [Calculate MD5]")
+    print("[3] [Scan for Rule]")
+    print("[0] [Exit]\n")
     task = input("Choose Operation : ")
+    if(task == '0'):
+        quit()
     if(task == '1'):
         file = input("Enter File Path : ")
         print(claculate_obj.hex(file))
@@ -57,7 +60,12 @@ def container():
         print(claculate_obj.md5_sum(file))
     if(task == '3'):
         rule_file = input("Enter rule file name : ")
+        print('[+] Scanning Started')
         rule_code = open(rule_file, "r")
         code = rule_code.read()
         exec(code)
-container()
+        print('[+] Scanning Completed')
+try:
+    container()
+except:
+    print("[-] An error occured please check your files is set properly")
